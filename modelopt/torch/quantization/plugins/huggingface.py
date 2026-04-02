ee95177dbcf0018ab0ef10952d9c90cf6cd8bcbc
ajrasane <131806219+ajrasane@users.noreply.github.com>
2026-04-02 00:04:44 +0000

    Remove manual registration of sparse moe block
    Signed-off-by: ajrasane <131806219+ajrasane@users.noreply.github.com>

--- modelopt/torch/quantization/plugins/huggingface.py
+++ modelopt/torch/quantization/plugins/huggingface.py
@@ -1180,19 +1180,6 @@ except ImportError:
     pass
 
 
-try:
-    from transformers.models.qwen3_omni_moe.modeling_qwen3_omni_moe import (
-        Qwen3OmniMoeThinkerTextSparseMoeBlock,
-    )
-
-    if Qwen3OmniMoeThinkerTextSparseMoeBlock not in QuantModuleRegistry:
-        QuantModuleRegistry.register(
-            {Qwen3OmniMoeThinkerTextSparseMoeBlock: "hf.Qwen3OmniMoeThinkerTextSparseMoeBlock"}
-        )(_QuantSparseMoe)
-except ImportError:
-    pass
-
-
 class _QuantGptOssExperts(_QuantFunctionalMixin):
     """Quantized wrapper for `transformers.GptOssExperts`.
 
